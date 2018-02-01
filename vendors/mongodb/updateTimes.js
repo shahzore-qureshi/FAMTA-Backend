77941904f585ce79a15f1340c071eee2f53d3a4e
Shahzore Qureshi <shahzore.qureshi@gmail.com>
2018-02-01 04:36:08 +0000

    Combined both update scripts into one

--- vendors/mongodb/updateTimes.js
+++ vendors/mongodb/updateTimes.js
@@ -18,31 +18,60 @@ const Promise = require("bluebird");
      
     let stationData = await client.db(dbName).collection('subway_stations').find({})
     let stationArray = await stationData.toArray()
-    let stationToTimeMaps = await Promise.map(stationArray, subwayStation => {
-      return mtaHelper.getSubwayStationToSubwayTimeMap(subwayStation.id)
+    let stationMaps = await Promise.map(stationArray, subwayStation => {
+      return mtaHelper.getSubwayStationServicesTimeMap(subwayStation.id)
     }, { concurrency: 1 })
 
+    //UPDATE STATIONS WITH SERVICES
+    let updatedStations = await Promise.map(stationMaps, stationMap => {
+        if(stationMap != null) {
+          return client.db(dbName).collection('subway_stations').findOneAndUpdate(
+            { id: stationMap.stationId },
+            { $set: { service_ids: stationMap.serviceIds } },
+            { returnOriginal: false }
+          )
+        } else { return Promise.resolve() }
+    }, { concurrency: 1 })
+
+    let stationsWithAtLeastOneService = []
+    updatedStations.forEach(subwayStation => {
+      if(subwayStation.value.service_ids.length > 0) {
+        stationsWithAtLeastOneService.push(subwayStation.value)
+      }
+    })
+
+    await client.db(dbName).collection('subway_stations_temp').insertMany(stationsWithAtLeastOneService)
+    await client.db(dbName).collection('subway_stations').drop()
+    await client.db(dbName).collection('subway_stations_temp').rename('subway_stations')
+
+    let checkStationData = await client.db(dbName).collection('subway_stations').find({})
+    let checkStationArray = await checkStationData.toArray()
+    console.log(checkStationArray)
+
+
+    //UPDATE TIMES
+
     let times = []
-    for(stationToTimeMap of stationToTimeMaps) {
-      if(stationToTimeMap.serviceIds) {
-        for(serviceId of stationToTimeMap.serviceIds) {
-          let northBoundTrains = stationToTimeMap.N.filter(train => train.routeId == serviceId)
+    for(stationMap of stationMaps) {
+      if(stationMap.serviceIds) {
+        for(serviceId of stationMap.serviceIds) {
+          let northBoundTrains = stationMap.N.filter(train => train.routeId == serviceId)
           for(train of northBoundTrains) {
             if(train.arrivalTime == null) train.arrivalTime = 1516957620
             train.arrivalTime *= 1000
             times.push({
-              station_id: stationToTimeMap.stationId,
+              station_id: stationMap.stationId,
               service_id: serviceId,
               bound_id: "N",
               arrival_time: train.arrivalTime
             })
           }
-          let southBoundTrains = stationToTimeMap.S.filter(train => train.routeId == serviceId)
+          let southBoundTrains = stationMap.S.filter(train => train.routeId == serviceId)
           for(train of southBoundTrains) {
             if(train.arrivalTime == null) train.arrivalTime = 1516957620
             train.arrivalTime *= 1000
             times.push({
-              station_id: stationToTimeMap.stationId,
+              station_id: stationMap.stationId,
               service_id: serviceId,
               bound_id: "S",
               arrival_time: train.arrivalTime
--- vendors/mta-gtfs/mtaHelper.js
+++ vendors/mta-gtfs/mtaHelper.js
@@ -2,29 +2,13 @@ const mtaGTFS = require('mta-gtfs')
 const mta = new mtaGTFS({ key: '3f2afc8d6b42c4ea0ba912d9abe5a28a' })
 const mtaMap = require('./mtaMap')
 
-const getSubwayStations =
-  mta.stop().then(results => {
-    let stationArray = Object.values(results)
-    for(station of stationArray) station.service_ids = []
-    return stationArray 
-  })
+const getSubwayStations = mta.stop().then(results => {
+  let stationArray = Object.values(results)
+  for(station of stationArray) station.service_ids = []
+  return stationArray 
+})
 
-const getSubwayStationToSubwayServicesMap = (stationId) => {
-  return mta.schedule(stationId, mtaMap.getFeedIdByStationId(stationId))
-    .then(result => {
-      if(result.schedule) {
-        let serviceSet = new Set()
-        result.schedule[stationId]["N"].forEach(train => serviceSet.add(train.routeId))
-        result.schedule[stationId]["S"].forEach(train => serviceSet.add(train.routeId))
-        return { stationId, serviceIds: [...serviceSet].sort() }
-      } else {
-        return { stationId, serviceIds: [] }
-      }
-    })
-    .catch(err => { return { stationId, serviceIds: [] } })
-}
-
-const getSubwayStationToSubwayTimeMap = (stationId) => {
+const getSubwayStationServicesTimeMap = (stationId) => {
   return mta.schedule(stationId, mtaMap.getFeedIdByStationId(stationId))
     .then(result => {
       if(result.schedule) {
@@ -46,6 +30,5 @@ const getSubwayStationToSubwayTimeMap = (stationId) => {
 
 module.exports = {
   getSubwayStations,
-  getSubwayStationToSubwayServicesMap,
-  getSubwayStationToSubwayTimeMap
+  getSubwayStationServicesTimeMap
 }
